45c25c481ff75f462b0794830dbb0f58c87f9278
Florence Nway <florencenway@Florences-MacBook-Air.local>
2020-03-04 22:21:15 +0000

    comparison

--- 07-js-comparisons-and-conditionals-exercise/index.js
+++ 07-js-comparisons-and-conditionals-exercise/index.js
@@ -4,6 +4,25 @@ const userValue = "15";
 const password = "JSisKool888";
 const passwordConfirmation = "JSisCool888";
 
+let passwordMatchesConfirmation;
+let passwordMessage;
+
 const sortOrder = "ALPHABETICAL";
+let fruits;
+
 
 // ADD YOUR CODE BELOW
+
+const targetMatchesCoercedUserValue = target == userValue;
+const targetMatchesUserValue = target === userValue;
+
+passwordMatchesConfirmation = targetMatchesUserValue;
+
+if(passwordMatchesConfirmation == false) {
+    passwordMessage = "Oops, your password and confirmation do not match!"
+}
+
+if(sortOrder == "ALPHABETICAL") {
+    fruits = "banana, cherry, watermelon"
+}
+
