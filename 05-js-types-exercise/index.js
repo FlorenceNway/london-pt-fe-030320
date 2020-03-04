c02d99b135dccfd428157602a3d96d9582a29258
Florence Nway <florence@Florences-iMac.local>
2020-03-04 16:41:13 +0000

    types

---
+++ 05-js-types-exercise/index.js
@@ -0,0 +1,19 @@
+let greeting = "Hello";
+let name = "Florence";
+
+let phrase = `${greeting} ${name}`;
+
+let price = 10.5;
+
+let quantity = 20;
+
+let total = price * quantity;
+ let age1 = 20;
+ let age2 = 20;
+ let age3 = 20;
+ let averageAge = (age1 + age2 + age3)/3;
+
+ let loggedIn = true;
+ let adminUser = false;
+
+ let missingInfo;
