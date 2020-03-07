40e394160ab1f469dd68a8b99d684c3ba1e62244
Florence Nway <florencenway@Florences-MacBook-Air.local>
2020-03-07 20:58:27 +0000

    13 completed

--- 13-js-function-return-exercise/index.js
+++ 13-js-function-return-exercise/index.js
@@ -4,7 +4,9 @@
  * Create a function `greet` which takes your name as a parameter
  * and return "Hello 'your_name'!"
  */
-
+function greet(name) {
+    return `Hello ${name}!`;
+}
 /**
  * Exercise 2
  *
@@ -12,6 +14,13 @@
  * and returns "Welcome!" if the age is 18+ and "Sorry, not allowed."
  * if the age is less than 18
  */
+function loginValidation(age) {
+    if(age >= 18) {
+        return "Welcome!"
+    }else {
+        return "Sorry, not allowed."
+    }
+}
 
 /**
  * Exercise 3
@@ -20,16 +29,35 @@
  * and return the least one.
  */
 
+ function minNumber(num1, num2) {
+    if(num1 > num2) {
+        return num2
+    }else {
+        return num1
+    }
+ }
+
 /**
  * Exercise 4
  *
  * Create a function `randomNumber` which returns an integer between
  * 1 and 10, fraction should be rounded to lower integer
  */
+ 
 
+ function randomNumber() {
+     let randomNumber = (Math.random() * 10) + 1
+    return Math.floor(randomNumber);
+ }
+
+ 
 /**
  * Exercise 5
  *
  * Create a function `getOppositeNumber` that takes a number as a param and
  * returns an opposite number
  */
+
+ function getOppositeNumber(num) {
+        return num * -1;
+ }
