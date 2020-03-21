92b768e2db7b45a672abd75f312144d353ac7803
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-03-21 01:51:43 +0000

    18 - completed

--- 18-creating-arrays-exercise/index.js
+++ 18-creating-arrays-exercise/index.js
@@ -4,7 +4,7 @@
  * Create a variable "wildAnimals" and and 'tiger', 'rabbit',
  * 'panda' and 'giraffe' into it as strings
  */
-
+const wildAnimals = ['tiger', 'rabbit', 'panda', 'giraffe']
 
 /**
  * Exercise 2
@@ -12,6 +12,7 @@
  * Create a variable "pets" and and 'cat', 'dog' and 'hamster'
  * into it as strings(must contains dog)
  */
+const pets = ['cat', 'dog', 'hamster']
 
 
 /**
@@ -20,7 +21,7 @@
  * Create a variable "animals". It should include all wildAnimals
  * and all pets. Find the way to concatenate your arrays with JS
  */
-
+ const animals = wildAnimals.concat(pets)
 
 /**
  * Exercise 4
@@ -28,7 +29,11 @@
  * create a dogIndex variable and by using "findIndex" JS method
  * find dog index in "animals" array
  */
-
+function findDogindex(animal) {
+        
+        return animal == 'dog'
+}
+const dogIndex = animals.findIndex(findDogindex)
 
 /**
  * Exercise 5
@@ -36,14 +41,21 @@
  * create a pandaIndex variable and by using "indexOf" JS method
  * find dog index in "animals" array
  */
+let Animals = ['cat', 'hamster', 'dog' ]
 
+const pandaIndex = Animals.indexOf('dog')
 
 /**
  * create a variable with name "numbers"  which will be an array
  * and contain numbers from 1 to 9(ascending) and from 9 to 1(descending)
  * (each number should be twice in this array).
  */
-
+const ascendnumbers = [1,2,3,4,5,6,7,8,9];
+const descendnumbers = [9,8,7,6,5,4,3,2,1];
+//descendnumbers.reverse();
+//console.log(ascendnumbers)
+const numbers = ascendnumbers.concat(descendnumbers);
+console.log(numbers)
 
 /**
  * Exercise 6
@@ -51,8 +63,10 @@
  * Create variable "first4DigitIndex" and from "numbers"
  * find index of first instance of digit 4
  */
+const first4DigitIndex = numbers.indexOf(4);
 
 
+ 
 /**
  * Exercise 7
  *
@@ -60,13 +74,14 @@
  * find index of last instance of digit 4
  */
 
-
+const last4DigitIndex = numbers.lastIndexOf(4);
 /**
  * Exercise 8
  *
  * Create variable "hasSeven" check does "numbers"
  * contains 7
  */
+const hasSeven = numbers.includes(7)
 
 
 /**
@@ -75,7 +90,7 @@
  * Create variable "hasTwenty" check does "numbers"
  * contains 20
  */
-
+const hasTwenty = numbers.includes(20)
 
 /**
  * Exercise 10
@@ -83,7 +98,10 @@
  * Create variable "moreThanFour" returns first number
  * "numbers" that are more than 4
  */
-
+function morethanfour (number) {
+        return number > 4
+}
+const moreThanFour = numbers.find(morethanfour)
 
 /**
  * Exercise 11
@@ -91,4 +109,7 @@
  * Create variable "moreThanSevenIndex" returns first number
  * "numbers" that are more than 7
  */
-
+function morethanseven (number) {
+        return number > 7
+}
+const moreThanSevenIndex = numbers.findIndex(morethanseven)
