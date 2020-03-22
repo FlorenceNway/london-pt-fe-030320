123564ca62a401895934f7e490dc536eb98e4122
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-03-22 19:52:35 +0000

    25 - completed

--- 25-while-loop-exercise/index.js
+++ 25-while-loop-exercise/index.js
@@ -8,6 +8,13 @@ let count = 10; // DO NOT TOUCH THIS LINE :)
  * "Remaining: X" where X is a number. When {count} will
  * be equal to 0 log message "Go!"
  */
+function countdown() {
+    while(count > 0){
+        console.log(`Remaining: ${count}`)
+        count --;
+    }
+        console.log("Go!")
+}
 
 // ==========================
 
@@ -18,6 +25,13 @@ let count = 10; // DO NOT TOUCH THIS LINE :)
  * be the same as {countdown} the only difference is
  * that it takes any number as a param and count from that point
  */
+function countdownWithParam(countdown) {
+    while(countdown > 0){
+        console.log(`Remaining: ${countdown}`)
+        countdown --;
+    }
+        console.log("Go!")
+}
 
 // ==========================
 let ruler = [];
@@ -29,6 +43,15 @@ let ruler = [];
  * The result of calling {rulerConstructor} is that {ruler}
  * will be [0,10,20,30,40,50,60,70,80,90,100]
  */
+function rulerConstructor() {
+    let i = 0;
+    while( i <= 100) {
+        ruler.push(i);
+        i += 10;
+    }
+    console.log(ruler)
+    return ruler;
+}
 
 // ==========================
 
@@ -39,3 +62,12 @@ let ruler = [];
  * "Neighbour" to the array until you have at least 5 guests.
  * Without using an if statement!
  */
+function partyPadding(guests) {
+    let counter = 5 - guests.length;
+   
+    for(let i = 0; i < counter; i++) {
+        guests.push('Neighbour')
+    }
+    
+    return guests;
+}
