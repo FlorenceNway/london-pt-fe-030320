cd3f92f0b2c381a4181fd6be57a51158336088ee
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-03-22 23:50:10 +0000

    26 - completed

--- 26-for-of-loop-exercise/index.js
+++ 26-for-of-loop-exercise/index.js
@@ -10,13 +10,33 @@
  * a new array with these integers. If there is no integers it should return
  * empty array.
  */
-
+function getIntegersOnly(mixArr) {
+    let integerArr = [];
+    for(let item of mixArr) {
+        if(typeof(item) === "number") {
+            integerArr.push(item);
+        }
+    }
+    return integerArr;
+}
 /**
  * Exercise 2
  * create a function {getEvenNumbers}, which takes an array of integers
  * and returns a new array with even numbers only, if there is no even
  * integers it should return an empty array
  */
+function getEvenNumbers(mixArr) {
+    let evenArr = [];
+    for(let item of mixArr) {
+        if(item != 0) {
+            if(item % 2 == 0) {
+                evenArr.push(item);
+            }
+        } 
+    }
+    console.log('Even:',evenArr)
+    return evenArr;
+}
 
 /**
  * Exercise 3
@@ -26,6 +46,17 @@
  * a new array with even numbers only, if there is no even integers it
  * should return an empty array
  */
+function getEvenNumbersFromMixedArray(mixArr) {
+    let evenArr = [];
+    for(let item of mixArr) {
+        if(item != 0 && typeof(item) == 'number' ) {
+            if(item % 2 == 0) {
+                evenArr.push(item);
+            }
+        } 
+    }
+    return evenArr;
+}
 
 /**
  * Exercise 4
@@ -33,6 +64,18 @@
  * and returns a new array with odd numbers only, if there is no odd
  * integers it should return an empty array
  */
+function getOddNumbers(Arr) {
+    let oddArr = [];
+    for(let item of Arr) {
+       
+        if(item % 2 == 1) {
+            oddArr.push(item);
+        }
+        
+    }
+    console.log('Odd: ',oddArr)
+    return oddArr;
+}
 
 /**
  * Exercise 5
@@ -43,3 +86,17 @@
  * If your param is [1,2,3,4,5] the result should be
  * [2,1,4,3,6]
  */
+function evenOddTransform(integerArr) {
+    let Arr = [];
+    for(let item of integerArr) {
+        if(item % 2 == 0) {
+            item = item - 1;
+            Arr.push(item);
+        }
+        else if(item % 2 == 1) {
+            item = item + 1;
+            Arr.push(item);
+        }
+    }
+    return Arr;
+}
