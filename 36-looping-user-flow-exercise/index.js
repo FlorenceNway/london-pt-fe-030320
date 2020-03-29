1cf8a17b24fe8b1abf9902dc49edd4052e91dc1f
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-03-29 23:37:39 +0100

    36 - completed (big smile :D)

--- 36-looping-user-flow-exercise/index.js
+++ 36-looping-user-flow-exercise/index.js
@@ -1,4 +1,5 @@
 // install and  import "readline-sync" npm package before you do exercises
+const readlineSync = require("readline-sync");
 
 let selectedItems = {
 	book: "",
@@ -35,7 +36,32 @@ const countries = [
 	"Denmark",
 	"Netherland"
 ];
+//---------------------------------------------
+const user = {}
+const mainmenu = ['Books', 'Movies', 'Next Destination']
 
+let menu = readlineSync.keyInSelect(mainmenu,`Pick a category`, {cancel: 'Exit'})
+
+if(menu == -1) {
+	exit();
+}else {
+	switch(menu) {
+		case 0 : Books()
+		case 1 : Movies()
+		case 2 : Destinations()
+		default: exit()
+	}
+}
+
+
+function selectedCategories(category, key) {
+	for(let index = 0; index < category.length; index++) {
+		if (index == user[key]) {
+			selectedItems[key] = category[index]
+		}
+	}
+}
+//=============================================
 /**
  * Exercise 1
  *
@@ -45,6 +71,19 @@ const countries = [
  *
  * NOTE: You need to add option to go back, to main menu
  */
+function Books() {
+	user['book'] = readlineSync.keyInSelect(books,`Pick a book you want to read ?`,{cancel: 'Go to main menu'})
+	console.log(typeof user['book'])
+	
+	if(user['book'] === -1) {
+		console.log('here')
+		mainMenu()
+	} else {
+		selectedCategories(books, 'book')
+	}
+}
+
+
 
 /**
  * Exercise 2
@@ -55,6 +94,16 @@ const countries = [
  *
  * NOTE: You need to add option to "go back", to main menu
  */
+function Movies() {
+	user['movie'] = readlineSync.keyInSelect(movies,`Pick a movie you want to watch ?`,{cancel: 'Go to main menu'})
+	
+	if(user['movie'] === -1) {
+		console.log('here')
+		mainMenu()
+	} else {
+		selectedCategories(movies, 'movie')
+	}
+}
 
 /**
  * Exercise 3
@@ -66,6 +115,16 @@ const countries = [
  * NOTE: You need to add option to go back, to main menu
  */
 
+function Destinations() {
+	user['nextTrip'] = readlineSync.keyInSelect(countries,`Pick a country you want to travel ?`,{cancel: 'Go to main menu'})
+	
+	if(user['Next destination'] === -1) {
+		console.log('here')
+		mainMenu()
+	} else {
+		selectedCategories(countries, 'nextTrip')
+	}
+}
 /**
  * Exercise 4
  *
@@ -73,3 +132,28 @@ const countries = [
  * so user can pick one. User also should have the option "Exit".
  * When the user pick "Exit", log selected items.
  */
+
+function mainMenu() {
+	menu = readlineSync.keyInSelect(mainmenu,`Pick a category`, {cancel: 'Exit'})
+	if(menu == -1) {
+		exit();
+	}else {
+		switch(menu) {
+			case 0 : Books()
+			case 1 : Movies()
+			case 2 : Destinations()
+		}
+	}
+}
+
+function exit() {
+	console.log('Your choices are as follow: ')
+	for(key in selectedItems) {
+		if(selectedItems.hasOwnProperty(key)){
+			console.log(key ,': ',selectedItems[key])
+		}
+	}
+}
+
+
+	
