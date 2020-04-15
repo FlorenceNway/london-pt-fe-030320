e06b79b865d4a4ad064574190ee0f93eae2db485
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-04-15 01:35:58 +0100

    44 - completed

--- 44-budget-tracker-exercise/index.js
+++ 44-budget-tracker-exercise/index.js
@@ -1,32 +1,73 @@
 const productsContainer = document.querySelector("#products");
 
-const product_list = JSON.parse(localStorage.products)
-
+const product_list = JSON.parse(localStorage.products);
 
 const createProduct = () => {
-
-    product_list.forEach(product => {
-
-        const product_div = `<img src=${product.img} />
+  product_list.forEach((product) => {
+    const product_div = `<img src=${product.img} />
                             <h3>${product.name}</h3>
-                            <p>${product.price}</p> `
+                            <p>${product.price}</p> `;
 
-        const div = document.createElement('div')
-        
-        const select = document.createElement('select')
-        for(let quantity = 0; quantity < product.max_quantity; quantity++) {
-            const option = document.createElement('option')
-            option.value = quantity;
-            option.innerText = quantity;
-            select.append(option)
-        } 
-        
-        div.innerHTML = product_div
-        div.append(select)
+    const div = document.createElement("div");
+    const select = document.createElement("select");
+    select.id = 'select_' + product.id
+    select.setAttribute('onchange',`selectQuantity(${product.id})`)
 
-        productsContainer.append(div)
-    })
-     
+    for (let quantity = 0; quantity < product.max_quantity; quantity++) {
+      const option = document.createElement("option");
+      option.value = quantity;
+      option.innerText = quantity;
+      select.append(option);
+    }
+
+    div.innerHTML = product_div;
+    div.append(select);
+
+    productsContainer.append(div);
+  });
+};
+
+const totalAmount = [];
+
+const selectQuantity = (id) => {
+    const select = document.querySelector(`#select_${id}`)
+    let quantity = select.value
+    let price = product_list[id - 1].price
+    let each_total = calculateAmount(price ,quantity)
+    totalAmount.push(each_total)
+    displayBalance()
 }
 
-createProduct()
+
+const remaining_balance = 50
+
+const calculateAmount = (price , quantity) => {
+    let each_total = price * quantity
+    return each_total
+    
+}
+
+const displayBalance = () => {
+    let sum = totalAmount.reduce((accumulator, currentval) => {
+        return accumulator += currentval
+    },0)
+   
+    let balance = remaining_balance - sum
+
+    const span = document.querySelector('#remaining span')
+    const error_div = document.createElement('div')
+
+    if(sum > remaining_balance) {
+        const main_div = document.querySelector('#cart')
+        
+        error_div.className = 'error'
+        error_div.innerText = 'Not enough money left for that!'
+        main_div.append(error_div)
+        
+    }else if(sum < remaining_balance) {
+        error_div.remove()
+    }
+    span.innerText = balance.toFixed(2)
+}
+
+createProduct();
