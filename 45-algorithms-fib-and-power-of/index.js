077412ccf3ab3dd877e81ec10e325c538e3fde8c
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-04-18 23:19:27 +0100

    45 - completed (refactored)

--- 45-algorithms-fib-and-power-of/index.js
+++ 45-algorithms-fib-and-power-of/index.js
@@ -3,51 +3,42 @@
  *
  * create a function {generateFibonacci} which will take a parameter
  * that represents a length of an array that needs to be generated
- * 
+ *
  * The array must be a Fibonacci sequence:
- * 
- * The next number is found by adding up the two numbers before it, 
+ *
+ * The next number is found by adding up the two numbers before it,
  * always starting with 0 and 1.
  *
  * Ex: generateFibonacci(5) // return [0, 1, 1, 2, 3];
  */
 const generateFibonacci = (lenghtOfArray) => {
-    const result = [0]
-        for(let i = 0; i < lenghtOfArray - 1 ; i++) {
-            if(i == 0) {
-                result.push(1)
-            }
-            else {
-                let sum = result[i-1] + result[i]
-                result.push(sum)
-            }
-        }
-    
-    return result
-}
+  const result = [];
+  for (let i = 0; i < lenghtOfArray; i++) {
+    i < 2 ? result.push(i) : result.push(result[i - 2] + result[i - 1]);
+  }
+
+  return result;
+};
 
 /**
  * Exercise 2
  *
  * create a function {isPowerOf} which takes 2 arguments:
- * 
+ *
  * number – a number
  * primeNumber – a number (a prime number)
- * 
+ *
  * return a Boolean if your number is a power of primeNumber
  *
  * Ex: isPowerOf(27, 3) // return true;
  */
 
 const isPowerOf = (number, primeNumber) => {
-    let multiply = 1;
-    for(let i = 0 ; i < primeNumber; i++) {
-        multiply *= primeNumber
-    }
+  let multiply = 1;
 
-    if(multiply == number) {
-        return true
-    }else {
-        return false
-    }
-}
+  for (let i = 0; i < primeNumber; i++) {
+    multiply *= primeNumber;
+  }
+
+  return multiply == number ? true : false;
+};
