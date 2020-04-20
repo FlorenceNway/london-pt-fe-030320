a1e87c04602a2f2c68bb7d663321db861a9fb16f
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-04-21 00:37:26 +0100

    46 - completed

---
+++ 46-algorithms-rot-13/index.js
@@ -0,0 +1,46 @@
+const encrypt = document.querySelector('#encrypt') 
+const decrypt = document.querySelector('#decrypt')
+const text = document.querySelector('textarea')
+const resultEl = document.querySelector(".result");
+
+const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+encrypt.addEventListener('click', () => {
+    rot13()
+})
+
+decrypt.addEventListener('click', () => {
+    rot13()
+})
+
+const rot13 = () => {
+    let input = [...text.value]
+
+    input.forEach((char,index) => {
+        
+        if(alphabet.includes(char.toUpperCase())){
+            let charIndex = alphabet.indexOf(char.toUpperCase())
+            
+            if(charIndex > alphabet.length - 14) {
+                if(char == char.toUpperCase()){
+                    input[index] = alphabet[charIndex - 13]
+                }else {
+                    input[index] = alphabet[charIndex - 13].toLowerCase()
+                }
+                
+            }else {
+                if(char == char.toUpperCase()){
+                    input[index] = alphabet[charIndex + 13]
+                }else {
+                    input[index] = alphabet[charIndex + 13].toLowerCase()
+                }
+            }
+            
+        }else {
+            input[index] = char
+        }
+    })
+    const str = input.toString()
+    text.value = str.replace(/,/g,'')
+    resultEl.innerHTML=text.value
+}
