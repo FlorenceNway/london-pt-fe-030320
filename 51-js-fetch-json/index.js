f41645824774d2e04972d49bdb7431139afed742
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-07 04:13:07 +0100

    51 - completed

--- 51-js-fetch-json/index.js
+++ 51-js-fetch-json/index.js
@@ -16,11 +16,26 @@ const input = document.querySelector("input");
  * a URL as an argument and sends a GET request.
  * When you get a response, return an array of facts.
  */
+const url = 'https://cat-fact.herokuapp.com/facts'
 
-<<<<<<< HEAD
+// const fetchData = (url) => {
+// 	fetch(url)
+// 	.then((response) => response.json())
+// 	.then((bookData) => {
+//     bookData.items.forEach((book) => {
+//       console.log(book.volumeInfo.title);
+//     });
+//   });
 
-=======
->>>>>>> 313149d5869877fe032d44c9a1310cfabb3e670f
+// }
+
+const fetchData = async(url) => {
+	const data = await fetch(url)
+	.then((response) => response.json())
+
+	return data;
+}
+
 
 /**
  * Description of the application:
@@ -29,7 +44,31 @@ const input = document.querySelector("input");
  * 1. click on a button "Get random facts"
  * 2. view 3 random facts in ".result" element
  */
-<<<<<<< HEAD
 
-=======
->>>>>>> 313149d5869877fe032d44c9a1310cfabb3e670f
+const render_fact = (text, authorFirstName, authorLastName) => {
+
+	const eachFact = 
+	`<li>
+		<p class="fact">${text}</p>
+		<p class="author">${authorFirstName} ${authorLastName}</p>
+	</li>`
+
+	result.innerHTML += eachFact
+}
+
+button.addEventListener('click',async() => {
+	result.innerHTML = ''
+	const random3Facts = []
+
+	let facts = await fetchData(url)
+	facts = facts.all
+
+	for(let i = 1; i < 4; i++) {
+		let randomfact = facts[Math.floor(Math.random() * facts.length)]
+		random3Facts.push(randomfact)
+	}
+	
+	random3Facts.forEach(fact => {
+		render_fact(fact.text,fact.user.name.first, fact.user.name.last)
+	})
+})
