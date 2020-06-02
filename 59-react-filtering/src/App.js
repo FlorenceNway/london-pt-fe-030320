ab42888df0837eec0d3eeb172fb8ce0f671d54b4
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-06-02 17:39:40 +0100

    filter with company name

--- 59-react-filtering/src/App.js
+++ 59-react-filtering/src/App.js
@@ -18,8 +18,32 @@ import "./App.css";
  */
 
 const App = () => {
+
+	const [contacts, setContacts] = useState([])
+	const [selectedContact, setSelectedContact] = useState("All");
+
+	fetch("http://localhost:3001/contacts").then((response) => response.json()).then(res => setContacts(res));
+
+	const contactsToRender =
+		selectedContact === "All"
+			? contacts
+			: contacts.filter((contact) => contact.company.match(selectedContact));
+
+	const handleChange = (e) => {
+		setSelectedContact((e.target.value).toLowerCase())
+	}
+
 	return (
 		<div className="App">
+			<input type='text' onChange={handleChange} placeholder="Type the company name to search..."/>
+			<ul>
+				{contactsToRender.map((contact) => (
+						<li>
+							<p>{contact.name}</p>
+							<p className={"company"}>{contact.company}</p>
+						</li>
+				))}	
+			</ul>
 		</div>
 	);
 };
